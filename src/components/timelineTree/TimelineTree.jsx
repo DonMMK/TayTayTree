e65da7755136e5ee7473ef60c949eef7f90c5df4
don <misurakalu@gmail.com>
2024-02-19 22:45:31 +1000

    Update TimelineTree.jsx

--- src/components/timelineTree/TimelineTree.jsx
+++ src/components/timelineTree/TimelineTree.jsx
@@ -1,24 +1,81 @@
 import React from 'react';
 import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
-import GroupWorkIcon from '@mui/icons-material/GroupWork';
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
 import './TimelineTree.css';
+import taylorswift from '../assets/taylorswift.jpg';
+
+
 
 export function timelineTree() {
   return (
     <div className="timeline-wrapper">
       <VerticalTimeline>
+
       <VerticalTimelineElement
         className="vertical-timeline-element--work"
         contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
         contentArrowStyle={{ borderRight: '7px solid rgb(33, 150, 243)' }}
         date="2011 - present"
         iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-        icon={<GroupWorkIcon />}
+        icon={<MusicNoteIcon />}
       >
-        <h3 className="vertical-timeline-element-title">Creative Director</h3>
+        <h3 className="vertical-timeline-element-title">Midnights</h3>
         <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
         <p>Creative Direction, User Experience, Visual Design, Project Management, Team Leading</p>
+        <img src={taylorswift} alt="Album Cover" style={{ width: '20%', height: '20%' }} />
+      </VerticalTimelineElement>
+      <VerticalTimelineElement
+        className="vertical-timeline-element--work"
+        contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+        contentArrowStyle={{ borderRight: '7px solid rgb(33, 150, 243)' }}
+        date="2011 - present"
+        iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+        icon={<MusicNoteIcon />}
+      >
+        <h3 className="vertical-timeline-element-title">Evermore</h3>
+        <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
+        <p>Creative Direction, User Experience, Visual Design, Project Management, Team Leading</p>
+        <img src={taylorswift} alt="Album Cover" style={{ width: '20%', height: '20%' }} />
+      </VerticalTimelineElement>
+      <VerticalTimelineElement
+        className="vertical-timeline-element--work"
+        contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+        contentArrowStyle={{ borderRight: '7px solid rgb(33, 150, 243)' }}
+        date="2011 - present"
+        iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+        icon={<MusicNoteIcon />}
+      >
+        <h3 className="vertical-timeline-element-title">Folklore</h3>
+        <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
+        <p>Creative Direction, User Experience, Visual Design, Project Management, Team Leading</p>
+        <img src={taylorswift} alt="Album Cover" style={{ width: '20%', height: '20%' }} />
+      </VerticalTimelineElement>
+      <VerticalTimelineElement
+        className="vertical-timeline-element--work"
+        contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+        contentArrowStyle={{ borderRight: '7px solid rgb(33, 150, 243)' }}
+        date="2011 - present"
+        iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+        icon={<MusicNoteIcon />}
+      >
+        <h3 className="vertical-timeline-element-title">Lover</h3>
+        <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
+        <p>Creative Direction, User Experience, Visual Design, Project Management, Team Leading</p>
+        <img src={taylorswift} alt="Album Cover" style={{ width: '20%', height: '20%' }} />
+      </VerticalTimelineElement>
+      <VerticalTimelineElement
+        className="vertical-timeline-element--work"
+        contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+        contentArrowStyle={{ borderRight: '7px solid rgb(33, 150, 243)' }}
+        date="2011 - present"
+        iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+        icon={<MusicNoteIcon />}
+      >
+        <h3 className="vertical-timeline-element-title">Reputation</h3>
+        <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
+        <p>Creative Direction, User Experience, Visual Design, Project Management, Team Leading</p>
+        <img src={taylorswift} alt="Album Cover" style={{ width: '20%', height: '20%' }} />
       </VerticalTimelineElement>
 
       <VerticalTimelineElement
@@ -27,11 +84,12 @@ export function timelineTree() {
         contentArrowStyle={{ borderRight: '7px solid rgb(33, 150, 243)' }}
         date="2011 - present"
         iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-        icon={<GroupWorkIcon />}
+        icon={<MusicNoteIcon />}
       >
-        <h3 className="vertical-timeline-element-title">Creative Director</h3>
+        <h3 className="vertical-timeline-element-title">1989</h3>
         <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
         <p>Creative Direction, User Experience, Visual Design, Project Management, Team Leading</p>
+        <img src={taylorswift} alt="Album Cover" style={{ width: '20%', height: '20%' }} />
       </VerticalTimelineElement>
 
       <VerticalTimelineElement
@@ -40,11 +98,12 @@ export function timelineTree() {
         contentArrowStyle={{ borderRight: '7px solid rgb(33, 150, 243)' }}
         date="2011 - present"
         iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-        icon={<GroupWorkIcon />}
+        icon={<MusicNoteIcon />}
       >
-        <h3 className="vertical-timeline-element-title">Creative Director</h3>
+        <h3 className="vertical-timeline-element-title">Red</h3>
         <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
         <p>Creative Direction, User Experience, Visual Design, Project Management, Team Leading</p>
+        <img src={taylorswift} alt="Album Cover" style={{ width: '20%', height: '20%' }} />
       </VerticalTimelineElement>
 
       <VerticalTimelineElement
@@ -53,11 +112,41 @@ export function timelineTree() {
         contentArrowStyle={{ borderRight: '7px solid rgb(33, 150, 243)' }}
         date="2011 - present"
         iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-        icon={<GroupWorkIcon />}
+        icon={<MusicNoteIcon />}
       >
-        <h3 className="vertical-timeline-element-title">Creative Director</h3>
+        <h3 className="vertical-timeline-element-title">Speak Now</h3>
         <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
         <p>Creative Direction, User Experience, Visual Design, Project Management, Team Leading</p>
+        <img src={taylorswift} alt="Album Cover" style={{ width: '20%', height: '20%' }} />
+      </VerticalTimelineElement>
+
+
+      <VerticalTimelineElement
+        className="vertical-timeline-element--work"
+        contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+        contentArrowStyle={{ borderRight: '7px solid rgb(33, 150, 243)' }}
+        date="2011 - present"
+        iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+        icon={<MusicNoteIcon />}
+      >
+        <h3 className="vertical-timeline-element-title">Fearless</h3>
+        <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
+        <p>Creative Direction, User Experience, Visual Design, Project Management, Team Leading</p>
+        <img src={taylorswift} alt="Album Cover" style={{ width: '20%', height: '20%' }} />
+      </VerticalTimelineElement>
+
+      <VerticalTimelineElement
+        className="vertical-timeline-element--work"
+        contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+        contentArrowStyle={{ borderRight: '7px solid rgb(33, 150, 243)' }}
+        date="2011 - present"
+        iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+        icon={<MusicNoteIcon />}
+      >
+        <h3 className="vertical-timeline-element-title">Taylor Swift</h3>
+        <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
+        <p>Creative Direction, User Experience, Visual Design, Project Management, Team Leading</p>
+        <img src={taylorswift} alt="Album Cover" style={{ width: '20%', height: '20%' }} />
       </VerticalTimelineElement>
 
 
